ad6cfc629684817e0843711e7998a072e702ed03
Sijin35 <sijinsingh222@gmail.com>
2026-05-28 04:39:08 +0000

    Added setter and getter logic

--- Player.py
+++ Player.py
@@ -1,8 +1,60 @@
 #!/usr/bin/python3
 """Player Class"""
 
-from abc import abstractmethod
-
-
 class Player:
     """"""
+
+    MAX_ITEMS = 6
+
+    """Name of user/champion"""
+    @property
+    def name(self):
+        return self.__name
+    @name.setter(self, name):
+        self.__name = name
+
+    """Hp of user/champion"""
+    @property
+    def hp(self):
+        return self.__hp
+    @hp.setter
+    def hp(self, hp):
+        self.__hp = hp
+
+    """Mana of user/champion"""
+    @property
+    def mana(self):
+        return self.__mana
+    @mana.setter
+    def mana(self):
+        self.__mana = mana
+
+    """Current gold"""
+    @property
+    def gold(self):
+        return self.__gold
+    @gold.setter(self, gold):
+        if type(gold) is not int:
+            raise TypeError("Gold must be int")
+        if gold < 0:
+            raise ValueError("Gold cannot be negative")
+        self.__gold = gold
+
+    """Inventory status"""
+    @property
+    def inventory(self):
+        return self.__inventory
+    @inventory.setter
+    def inventory(self, inventory):
+        if not type(list):
+            raise TypeError("Not List")
+        if inventory > MAX_ITEMS:
+            raise ValueError("Cannot have more than 6 items at a time")
+            """Drop on ground maybe"""
+        self.__inventory = inventory
+
+    def __str__(self):
+        if self.__hp < 0:
+            return f"{name} has been pwned"
+        if self.__mana < mana: #placeholder, not 100% confident if logic is correct
+            return "Not enough mana"
