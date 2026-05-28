ea86328b14ddb101a0573c2fbe776064711d4da1
Sebastion Price <sebastionprice@gmail.com>
2026-05-28 14:40:30 +1000

    Add Implementaions for some base methods

--- Player.py
+++ Player.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """Player Class"""
 
+from Item_class import Item
 from abc import abstractmethod
 
 
 class Player:
     """"""
 
-    MAX_ITEMS = 6
+    __MAX_ITEMS = 6
 
     @abstractmethod
     def attack(self): ...
 
-    @abstractmethod
-    def buy(self): ...
+    def buy(self, item: Item):
+        if type(item) is not Item:
+            raise TypeError("item must be an Item object")
+        if item.price > self.gold:
+            raise BaseException("Not Enough Gold!!!")
 
     @abstractmethod
     def move(self): ...
