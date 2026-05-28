73414b8a4b26fefd8a91fe3c9f1bc2a4a85cbebc
Sijin35 <sijinsingh222@gmail.com>
2026-05-28 05:30:42 +0000

    Implemented initialisation

--- Player.py
+++ Player.py
@@ -6,6 +6,14 @@ class Player:
 
     __MAX_ITEMS = 6
 
+    def __init__(self, name, hp, mana, gold, inventory, attack_score):
+        self.name = name
+        self.hp = hp
+        self.mana = mana
+        self.gold = gold
+        self.inventory = inventory
+        self.attack_score = attack_score
+
     @abstractmethod
     def attack(self): ...
 
@@ -25,6 +33,7 @@ class Player:
     @property
     def name(self):
         return self.__name
+
     @name.setter(self, name):
         self.__name = name
 
@@ -32,6 +41,7 @@ class Player:
     @property
     def hp(self):
         return self.__hp
+
     @hp.setter
     def hp(self, hp):
         self.__hp = hp
@@ -40,6 +50,7 @@ class Player:
     @property
     def mana(self):
         return self.__mana
+
     @mana.setter
     def mana(self):
         self.__mana = mana
@@ -48,6 +59,7 @@ class Player:
     @property
     def gold(self):
         return self.__gold
+
     @gold.setter(self, gold):
         if type(gold) is not int:
             raise TypeError("Gold must be int")
@@ -67,6 +79,15 @@ class Player:
             raise ValueError("Cannot have more than 6 items at a time")
             """Drop on ground maybe"""
         self.__inventory = inventory
+
+    @property
+    def attack_score(self):
+        return self.__attack_score
+
+    @attack_score.setter(self, attack_score):
+        if type(attack_score) is not int:
+            raise TypeError:("Attack score should be an integer")
+        self.__attack_score = attack_score
 
     def __str__(self):
         if self.__hp < 0:
