82e8528f01ad84746be366f55e9d6e8418ba4e33
Sebastion Price <sebastionprice@gmail.com>
2026-05-31 03:07:03 +1000

    Add live resizing including mid-typeing

--- Map.py
+++ Map.py
@@ -108,28 +108,30 @@ class Map:
                 square.render(y, x, map_win)
         map_x = self.size * (Square().size + 1) + 5
         map_y = self.size * (Square().size + 1) + 1
-        self.refresh_map(map_win)
+        self.refresh_map()
         return map_x, map_y
 
     def launch_game(self) -> None:
         """"""
         self.__screen_size(self.stdscr)
-        map_win, dash_win, prompt_win = self.__setup_pads(*self.__get_win_sizes())
+        self.map_win, self.dash_win, self.prompt_win = self.__setup_pads(
+            *self.__get_win_sizes()
+        )
         while True:
             curses.update_lines_cols()
-            self.__resize_pads(map_win, dash_win, prompt_win, *self.__get_win_sizes())
-            map_win.clear()
-            dash_win.clear()
-            self.print_map(map_win)
-            self.render_dash(0, 0, dash_win)
-            self.refresh_dash(dash_win)
-            command = self.__prompt(f"{self.name} >> ", prompt_win)
+            self.__resize_pads(*self.__get_win_sizes())
+            self.map_win.clear()
+            self.dash_win.clear()
+            self.print_map(self.map_win)
+            self.render_dash(0, 0, self.dash_win)
+            self.refresh_dash()
+            command = self.__prompt(f"{self.name} >> ", self.prompt_win)
             args = command.split()
             if args:
                 try:
                     match args[0]:
                         case "add":
-                            self.__add(*args[1:], prompt_win=prompt_win)
+                            self.__add(*args[1:], prompt_win=self.prompt_win)
                         case "buy":
                             self.__buy(*args[1:])
                         case "move":
@@ -137,19 +139,19 @@ class Map:
                         case "attack":
                             self.__attack(*args[1:])
                         case "help":
-                            self.__help(prompt_win, 1, 0)
+                            self.__help(self.prompt_win, 1, 0)
                         case _:
                             raise ValueError(f"Undefined command: {args[0]}")
                 except BaseException as err:
                     err_msg = f"Error: {err}\n"
-                    prompt_win.addstr(1, 0, err_msg)
-                    self.refresh_prompt(prompt_win)
+                    self.prompt_win.addstr(1, 0, err_msg)
+                    self.refresh_prompt()
 
     def __help(self, prompt_win: curses.window, p_y: int, p_x: int) -> None:
         # TODO: Update to match valid commands
         prompt_win.addstr(p_y, p_x, "Available commands:\n")
         prompt_win.addstr(p_y + 1, p_x, "help\n")
-        self.refresh_prompt(prompt_win)
+        self.refresh_prompt()
 
     def __add(self, team: str, player_name: str, prompt_win: curses.window) -> None:
         if type(team) is not str:
@@ -179,7 +181,7 @@ class Map:
                 self.squares[player.row][player.column].incoming(player)
         # TODO: add px, py for better control and reliablity
         prompt_win.addstr(1, 0, f"Team Red: {self.red}\nTeam Blue: {self.blue}")
-        self.refresh_prompt(prompt_win)
+        self.refresh_prompt()
 
     def __buy(self, item_name: str, player_name: str) -> None:
         player = self.__get_player(player_name)
@@ -269,10 +271,16 @@ class Map:
         type_limit = p_x
         while chr != "\n":
             prompt_win.move(p_y, p_x - 1)
-            self.refresh_prompt(prompt_win)
+            self.refresh_prompt()
             chr = prompt_win.getkey()
-            while chr in ["KEY_RIGHT", "KEY_LEFT", "KEY_UP", "KEY_BACKSPACE"]:
-                self.refresh_prompt(prompt_win)
+            while chr in [
+                "KEY_RIGHT",
+                "KEY_LEFT",
+                "KEY_UP",
+                "KEY_BACKSPACE",
+                "KEY_RESIZE",
+            ]:
+                self.refresh_prompt()
                 if chr == "KEY_RIGHT":
                     while chr == "KEY_RIGHT":
                         if left != "":
@@ -280,7 +288,7 @@ class Map:
                             left = left[1:]
                             p_x += 1
                         prompt_win.move(p_y, p_x - 1)
-                        self.refresh_prompt(prompt_win)
+                        self.refresh_prompt()
                         chr = prompt_win.getkey()
                 if chr == "KEY_LEFT":
                     while chr == "KEY_LEFT":
@@ -289,18 +297,25 @@ class Map:
                             command = command[:-1]
                             p_x -= 1
                         prompt_win.move(p_y, p_x - 1)
-                        self.refresh_prompt(prompt_win)
+                        self.refresh_prompt()
                         chr = prompt_win.getkey()
                 # TODO: Add coomand History
+                if chr == "KEY_RESIZE":
+                    while chr == "KEY_RESIZE":
+                        curses.update_lines_cols()
+                        self.__resize_pads(*self.__get_win_sizes())
+                        prompt_win.move(p_y, p_x - 1)
+                        self.refresh_prompt()
+                        chr = prompt_win.getkey()
                 if chr == "KEY_UP":
                     while chr == "KEY_UP":
                         prompt_win.move(p_y, p_x - 1)
-                        self.refresh_prompt(prompt_win)
+                        self.refresh_prompt()
                         chr = prompt_win.getkey()
                 if chr == "KEY_DOWN":
                     while chr == "KEY_DOWN":
                         prompt_win.move(p_y, p_x - 1)
-                        self.refresh_prompt(prompt_win)
+                        self.refresh_prompt()
                         chr = prompt_win.getkey()
                 if chr == "KEY_BACKSPACE":
                     while chr == "KEY_BACKSPACE":
@@ -314,7 +329,7 @@ class Map:
                             else:
                                 prompt_win.delch(p_y, p_x - 1)
                         prompt_win.move(p_y, p_x - 1)
-                        self.refresh_prompt(prompt_win)
+                        self.refresh_prompt()
                         chr = prompt_win.getkey()
                 if chr == "KEY_DC":
                     while chr == "KEY_DC":
@@ -325,7 +340,7 @@ class Map:
                                 prompt_win.clrtoeol()
                                 prompt_win.addstr(p_y, p_x - 1, left)
                         prompt_win.move(p_y, p_x - 1)
-                        self.refresh_prompt(prompt_win)
+                        self.refresh_prompt()
                         chr = prompt_win.getkey()
             if p_x < ((prompt_win.getmaxyx()[1]) - 1):
                 p_x += 1
@@ -341,7 +356,7 @@ class Map:
             command = command + left
             command += "\n"
         prompt_win.addstr(p_y, p_x, "\n")
-        self.refresh_prompt(prompt_win)
+        self.refresh_prompt()
         return command
 
     def __screen_size(self, stdscr: curses.window) -> None:
@@ -409,9 +424,6 @@ class Map:
 
     def __resize_pads(
         self,
-        map_win: curses.window,
-        dash_win: curses.window,
-        prompt_win: curses.window,
         map_h: int,
         map_w: int,
         dash_h: int,
@@ -431,25 +443,25 @@ class Map:
             raise ValueError("prompt_h cannot be 0")
         if prompt_w <= 0:
             raise ValueError("prompt_w cannot be 0")
-        map_win.resize(map_h, map_w)
-        dash_win.resize(dash_h, dash_w)
-        prompt_win.resize(prompt_h, prompt_w)
-        return map_win, dash_win, prompt_win
+        self.map_win.resize(map_h, map_w)
+        self.dash_win.resize(dash_h, dash_w)
+        self.prompt_win.resize(prompt_h, prompt_w)
+        return self.map_win, self.dash_win, self.prompt_win
 
-    def refresh_prompt(self, prompt_win: curses.window) -> None:
+    def refresh_prompt(self) -> None:
         """"""
         my, mx, dy, dx, py, px = self.__get_win_sizes()
-        prompt_win.refresh(0, 0, dy + 1, mx + 1, curses.LINES - 1, curses.COLS - 1)
+        self.prompt_win.refresh(0, 0, dy + 1, mx + 1, curses.LINES - 1, curses.COLS - 1)
 
-    def refresh_dash(self, dash_win: curses.window) -> None:
+    def refresh_dash(self) -> None:
         """"""
         my, mx, dy, dx, py, px = self.__get_win_sizes()
-        dash_win.refresh(0, 0, 0, mx + 1, dy, curses.COLS - 1)
+        self.dash_win.refresh(0, 0, 0, mx + 1, dy, curses.COLS - 1)
 
-    def refresh_map(self, map_win: curses.window) -> None:
+    def refresh_map(self) -> None:
         """"""
         my, mx, dy, dx, py, px = self.__get_win_sizes()
-        map_win.refresh(0, 0, 0, 0, my, mx)
+        self.map_win.refresh(0, 0, 0, 0, my, mx)
 
     def __fullprint(self, pad: curses.window, chr: str) -> None:
         for i in range(pad.getmaxyx()[0]):
@@ -461,11 +473,44 @@ class Map:
     ) -> None:
         my, mx, dy, dx, py, px = self.__get_win_sizes()
         self.__fullprint(map_win, "M")
-        self.refresh_map(map_win)
+        self.refresh_map()
         self.__fullprint(dash_win, "D")
-        self.refresh_dash(dash_win)
+        self.refresh_dash()
         self.__fullprint(prompt_win, "P")
-        self.refresh_prompt(prompt_win)
+        self.refresh_prompt()
+
+    @property
+    def map_win(self) -> curses.window:
+        """"""
+        return self.__map_win
+
+    @map_win.setter
+    def map_win(self, map_win: curses.window) -> None:
+        if not isinstance(map_win, curses.window):
+            raise ValueError("map_win must a window object")
+        self.__map_win = map_win
+
+    @property
+    def dash_win(self) -> curses.window:
+        """"""
+        return self.__dash_win
+
+    @dash_win.setter
+    def dash_win(self, dash_win: curses.window) -> None:
+        if not isinstance(dash_win, curses.window):
+            raise ValueError("dash_win must a window object")
+        self.__dash_win = dash_win
+
+    @property
+    def prompt_win(self) -> curses.window:
+        """"""
+        return self.__prompt_win
+
+    @prompt_win.setter
+    def prompt_win(self, prompt_win: curses.window) -> None:
+        if not isinstance(prompt_win, curses.window):
+            raise ValueError("prompt_win must a window object")
+        self.__prompt_win = prompt_win
 
     @property
     def squares(self) -> list:
